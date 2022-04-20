48bbe0fe61827e7073edc142506a21e07c1b53e0
shanecthomas <83098398+shanecthomas@users.noreply.github.com>
2022-04-20 12:35:52 -0700

    Create Graph.java

---
+++ src/Graph.java
@@ -0,0 +1,10 @@
+public class Graph<E>
+{
+    private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
+    private E[] labels; // labels[i] contains the label for vertex i  
+ 
+    public Graph(int n) {
+        edges = new boolean[n][n]; // All values initially false
+        labels = (E[]) new Object[n]; // All values initially null
+    }
+}
