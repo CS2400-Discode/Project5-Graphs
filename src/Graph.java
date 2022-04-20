215792a13b7bfd80c0f59a63555bba981d9b0e30
shanecthomas <83098398+shanecthomas@users.noreply.github.com>
2022-04-20 12:43:35 -0700

    getBreadthFirstTraversal(T origin)

--- src/Graph.java
+++ src/Graph.java
@@ -56,7 +56,34 @@ public class Graph<E>
     public int size() {
         return labels.length;
     }
-
-
-
+    
+    public QueueInterface<T> getBreadthFirstTraversal(T origin)
+    {
+        resetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<VertexInterface<T>>();
+        
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        traversalOrder.enqueue(origin);
+        vertexQueue.enqueue(originVertex);
+        
+        while (!vertexQueue.isEmpty())
+        {
+            VertexInterface<T> frontVertex = vertexQueue.dequeue();
+            Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
+            
+            while (neighbors.hasNext())
+            {
+                VertexInterface<T> nextNeighbor = neighbors.next();
+                if (!nextNeighbor.isVisited())
+                {
+                    nextNeighbor.visit();
+                    traversalOrder.enqueue(nextNeighbor.getLabel());
+                    vertexQueue.enqueue(nextNeighbor);
+                }
+            }
+        }
+        return traversalOrder;
+    }
 }
