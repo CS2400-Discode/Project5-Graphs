ff8e2ad121f5310dc79fe9772016cf1e7a9c2e25
shanecthomas <83098398+shanecthomas@users.noreply.github.com>
2022-04-21 09:28:27 -0700

    enqueue / Node class

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -8,4 +8,56 @@ public class LinkedQueue<T> implements QueueInterface<T>
     firstNode = null;
     lastNode = null;
   }
+  
+  public void enqueue(T newEntry)
+  {
+    Node newNode = new Node(newEntry, null);
+    
+    if (isEmpty())
+      firstNode = newNode;
+    else
+      lastNode.setNextNode(newNode);
+      
+    
+  }
+  
+  /***
+     * Creates nodes that can be used in other classes
+     */
+    private class Node
+    {
+        private T data;
+        private Node next;
+
+        private Node(T dataPortion)
+        {
+            this(dataPortion, null);
+        }
+
+        private Node(T dataPortion, Node nextNode)
+        {
+            data = dataPortion;
+            next = nextNode;
+        }
+
+        private void setData(T newData)
+        {
+            data = newData;
+        }
+
+        private T getData()
+        {
+            return data;
+        }
+
+        private Node getNextNode()
+        {
+            return next;
+        }
+
+        private void setNextNode(Node nextNode)
+        {
+            next = nextNode;
+        }
+    }
 }
