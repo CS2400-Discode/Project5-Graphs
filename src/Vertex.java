550b37b0c994f81aaa13c51b81698c0d6c91b7ba
Owen Lovett <olovett214@gmail.com>
2022-05-01 14:04:39 -0700

    added basic stuff to Vertex classes

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,3 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Vertex<T> implements VertexInterface<T> {
+
+    private T label;
+    private boolean visited;
+    private List<Vertex<T>> edgeList = new LinkedList<>();
+
+    public Vertex(T vertexLabel)
+    {
+        label = vertexLabel;
+        edgeList = new LinkedList<>();
+        visited = false;
+    }
+    
+    public T getLabel(){
+        return label;
+    }
+
+    public void visit(){
+        visited = true;
+    }
 
 }
--- src/VertexInterface.java
+++ src/VertexInterface.java
@@ -1,3 +1,9 @@
 public interface VertexInterface<T> {
-    
+
+    /** Gets this vertex's label.
+     @return  The object that labels the vertex. */
+    public T getLabel();
+
+    /** Marks this vertex as visited. */
+    public void visit();
 }
