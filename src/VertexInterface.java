44f2fb3e2b2b8ca24e5a8ec3c1b07d3ed6e9b438
Owen Lovett <olovett214@gmail.com>
2022-05-01 18:40:13 -0700

    added comment

--- src/VertexInterface.java
+++ src/VertexInterface.java
@@ -8,5 +8,8 @@ public interface VertexInterface<T> {
     /** Marks this vertex as visited. */
     public void visit();
 
+    /** Sees whether the vertex is marked as visited.
+       @return  True if the vertex is visited. */
     public boolean isVisited();
+    
 }
